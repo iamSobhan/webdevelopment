a6b1bf8e45378308ffda80f8bf23aa23573e735f
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2024-05-09 03:25:21 +0530

    js

---
+++ JavaScript/2Darray.js
@@ -0,0 +1,12 @@
+var a=[["Book","Pencil","Pen"],[52,48,44]];
+document.write(a[1][1]+"<br>");
+/*for(b=0;b<3;b++)
+     {
+       for(c=0;c<3;c++)
+     {
+         document.write(b+" "+c" "+a[b][c]+"|");
+     }
+         document.write("<br>");
+     }
+var d=["Book","Pencil","Pen"];
+document.write(indexof);     
